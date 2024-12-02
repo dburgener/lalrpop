d14b827870fa43dc16586fe1debe3da3d11ecab6
Daniel Burgener <Daniel.Burgener@microsoft.com>
2024-12-02 10:19:22 -0500

    Skip testing lexer doc when testing MSRV
    The msrv for logos was bumped to 1.74 in logos 0.14.3, so it no longer
    builds against 1.70

--- tools/ci.sh
+++ tools/ci.sh
@@ -6,7 +6,15 @@ export CARGO_INCREMENTAL=0
 export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
 
 cargo build --bin lalrpop --features pico-args
-cargo test --workspace
+
+EXCLUDE_FILES=${1:-""}
+
+if [ -n "${EXCLUDE_FILES}" ]; then
+	cargo test --workspace --exclude "$EXCLUDE_FILES"
+else
+	cargo test --workspace
+fi
+
 # Check the documentation examples separately so that the `lexer` feature specified in tests do not
 # leak into them
 cargo check -p calculator
